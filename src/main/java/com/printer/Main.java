09d2542827bc69abad0f4c8348c27da86721afc8
Kevin <kknguy24@gmail.com>
2019-05-10 02:15:22 -0500

    Starting

---
+++ src/main/java/com/printer/Main.java
@@ -0,0 +1,40 @@
+package com.printer;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Main extends Application {
+    private ConfigurableApplicationContext springContext;
+    private Parent root;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void init() throws Exception {
+        springContext = SpringApplication.run(Main.class);
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("/fxml/main.fxml"));
+        fxmlLoader.setControllerFactory(springContext::getBean);
+        root = fxmlLoader.load();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        springContext.close();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/com/printer/controller/mainController.java
@@ -0,0 +1,16 @@
+package com.printer.controller;
+
+import javafx.fxml.Initializable;
+import org.springframework.stereotype.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Controller
+public class mainController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
